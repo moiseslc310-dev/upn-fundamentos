fd1f7fb5deee8c77979d3c36a6455c5b60a6d736
moises <moiseslc310@gmail.com>
2026-06-25 18:55:01 -0500

    Ejercicio en WindowsForm - Semana 13 - 21Junio

---
+++ Semana13-21Mayo/Ejercicio1-PlanillaPorTurnos/Ejercicio1-PlanillaPorTurnos/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Ejercicio1_PlanillaPorTurnos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpMañana = new System.Windows.Forms.GroupBox();
+            this.grpTarde = new System.Windows.Forms.GroupBox();
+            this.grpNoche = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbM1 = new System.Windows.Forms.TextBox();
+            this.txbM2 = new System.Windows.Forms.TextBox();
+            this.txbM3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbN1 = new System.Windows.Forms.TextBox();
+            this.txbT2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txbN2 = new System.Windows.Forms.TextBox();
+            this.txbT1 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnDatosPrueba = new System.Windows.Forms.Button();
+            this.lblTurnoMayor = new System.Windows.Forms.Label();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.grpMañana.SuspendLayout();
+            this.grpTarde.SuspendLayout();
+            this.grpNoche.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMañana
+            // 
+            this.grpMañana.Controls.Add(this.txbM3);
+            this.grpMañana.Controls.Add(this.txbM2);
+            this.grpMañana.Controls.Add(this.txbM1);
+            this.grpMañana.Controls.Add(this.label5);
+            this.grpMañana.Controls.Add(this.label4);
+            this.grpMañana.Controls.Add(this.label3);
+            this.grpMañana.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpMañana.Location = new System.Drawing.Point(33, 115);
+            this.grpMañana.Name = "grpMañana";
+            this.grpMañana.Size = new System.Drawing.Size(244, 138);
+            this.grpMañana.TabIndex = 0;
+            this.grpMañana.TabStop = false;
+            this.grpMañana.Text = "Turno Mañana";
+            // 
+            // grpTarde
+            // 
+            this.grpTarde.Controls.Add(this.label6);
+            this.grpTarde.Controls.Add(this.txbT2);
+            this.grpTarde.Controls.Add(this.txbT1);
+            this.grpTarde.Controls.Add(this.label7);
+            this.grpTarde.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpTarde.Location = new System.Drawing.Point(297, 115);
+            this.grpTarde.Name = "grpTarde";
+            this.grpTarde.Size = new System.Drawing.Size(228, 138);
+            this.grpTarde.TabIndex = 1;
+            this.grpTarde.TabStop = false;
+            this.grpTarde.Text = "Turno Tarde";
+            // 
+            // grpNoche
+            // 
+            this.grpNoche.Controls.Add(this.label8);
+            this.grpNoche.Controls.Add(this.label9);
+            this.grpNoche.Controls.Add(this.txbN2);
+            this.grpNoche.Controls.Add(this.txbN1);
+            this.grpNoche.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpNoche.Location = new System.Drawing.Point(549, 115);
+            this.grpNoche.Name = "grpNoche";
+            this.grpNoche.Size = new System.Drawing.Size(228, 138);
+            this.grpNoche.TabIndex = 2;
+            this.grpNoche.TabStop = false;
+            this.grpNoche.Text = "Turno Noche";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ejercicio 1: Planilla por turnos";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(30, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(787, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese el sueldo de los trabajadores por turno. El programa calcula el gasto tot" +
+    "al, identifica el turno más costoso y ordena los sueldos de menor a mayor uzando" +
+    " burbuja";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sueldo 1: S/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sueldo 2: S/";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Sueldo 3: S/";
+            // 
+            // txbM1
+            // 
+            this.txbM1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbM1.Location = new System.Drawing.Point(125, 33);
+            this.txbM1.Name = "txbM1";
+            this.txbM1.Size = new System.Drawing.Size(100, 22);
+            this.txbM1.TabIndex = 1;
+            // 
+            // txbM2
+            // 
+            this.txbM2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbM2.Location = new System.Drawing.Point(125, 67);
+            this.txbM2.Name = "txbM2";
+            this.txbM2.Size = new System.Drawing.Size(100, 22);
+            this.txbM2.TabIndex = 1;
+            // 
+            // txbM3
+            // 
+            this.txbM3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbM3.Location = new System.Drawing.Point(125, 103);
+            this.txbM3.Name = "txbM3";
+            this.txbM3.Size = new System.Drawing.Size(100, 22);
+            this.txbM3.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Sueldo 1: S/";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 16);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Sueldo 2: S/";
+            // 
+            // txbN1
+            // 
+            this.txbN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbN1.Location = new System.Drawing.Point(112, 30);
+            this.txbN1.Name = "txbN1";
+            this.txbN1.Size = new System.Drawing.Size(100, 22);
+            this.txbN1.TabIndex = 1;
+            // 
+            // txbT2
+            // 
+            this.txbT2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbT2.Location = new System.Drawing.Point(109, 70);
+            this.txbT2.Name = "txbT2";
+            this.txbT2.Size = new System.Drawing.Size(100, 22);
+            this.txbT2.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(15, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Sueldo 1: S/";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(15, 76);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 16);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Sueldo 2: S/";
+            // 
+            // txbN2
+            // 
+            this.txbN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbN2.Location = new System.Drawing.Point(112, 70);
+            this.txbN2.Name = "txbN2";
+            this.txbN2.Size = new System.Drawing.Size(100, 22);
+            this.txbN2.TabIndex = 1;
+            // 
+            // txbT1
+            // 
+            this.txbT1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbT1.Location = new System.Drawing.Point(109, 33);
+            this.txbT1.Name = "txbT1";
+            this.txbT1.Size = new System.Drawing.Size(100, 22);
+            this.txbT1.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCalcular.Location = new System.Drawing.Point(44, 274);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(157, 36);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular y Ordenar";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnLimpiar.Location = new System.Drawing.Point(207, 274);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(131, 36);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnDatosPrueba
+            // 
+            this.btnDatosPrueba.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnDatosPrueba.Location = new System.Drawing.Point(344, 274);
+            this.btnDatosPrueba.Name = "btnDatosPrueba";
+            this.btnDatosPrueba.Size = new System.Drawing.Size(131, 36);
+            this.btnDatosPrueba.TabIndex = 5;
+            this.btnDatosPrueba.Text = "Datos de Prueba";
+            this.btnDatosPrueba.UseVisualStyleBackColor = false;
+            this.btnDatosPrueba.Click += new System.EventHandler(this.btnDatosPrueba_Click);
+            // 
+            // lblTurnoMayor
+            // 
+            this.lblTurnoMayor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblTurnoMayor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTurnoMayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnoMayor.Location = new System.Drawing.Point(490, 274);
+            this.lblTurnoMayor.Name = "lblTurnoMayor";
+            this.lblTurnoMayor.Size = new System.Drawing.Size(287, 36);
+            this.lblTurnoMayor.TabIndex = 6;
+            this.lblTurnoMayor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.ItemHeight = 16;
+            this.lstResultados.Location = new System.Drawing.Point(33, 379);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(744, 164);
+            this.lstResultados.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(44, 342);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 16);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Resultados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 575);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.lblTurnoMayor);
+            this.Controls.Add(this.btnDatosPrueba);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grpNoche);
+            this.Controls.Add(this.grpTarde);
+            this.Controls.Add(this.grpMañana);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 1: Planilla por turnos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMañana.ResumeLayout(false);
+            this.grpMañana.PerformLayout();
+            this.grpTarde.ResumeLayout(false);
+            this.grpTarde.PerformLayout();
+            this.grpNoche.ResumeLayout(false);
+            this.grpNoche.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMañana;
+        private System.Windows.Forms.GroupBox grpTarde;
+        private System.Windows.Forms.GroupBox grpNoche;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbM3;
+        private System.Windows.Forms.TextBox txbM2;
+        private System.Windows.Forms.TextBox txbM1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbT2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbN1;
+        private System.Windows.Forms.TextBox txbT1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txbN2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnDatosPrueba;
+        private System.Windows.Forms.Label lblTurnoMayor;
+        private System.Windows.Forms.ListBox lstResultados;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Semana13-21Mayo/Ejercicio1-PlanillaPorTurnos/Ejercicio1-PlanillaPorTurnos/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace Ejercicio1_PlanillaPorTurnos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Función para leer sueldos
+        // Esta función recibe varios TextBox, lee sus valores y los guarda en un arreglo.
+        private double[] LeerSueldos(TextBox[] cajas)
+        {
+            // Creamos un arreglo del mismo tamaño que la cantidad de TextBox recibidos.
+            double[] sueldos = new double[cajas.Length];
+            // Recorremos cada TextBox, convertimos su texto a double y lo guardamos.
+            for (int i = 0; i < cajas.Length; i++)
+            {
+                sueldos[i] = double.Parse(cajas[i].Text);
+            }
+            // Devolvemos el arreglo lleno.
+            return sueldos;
+        }
+
+        //Función para calcular total
+        //Esta función suma todos los sueldos de un turno.
+        private double CalcularTotal(double[] datos)
+        {
+            // Acumulador para sumar los sueldos.
+            double total = 0;
+            // Recorremos el arreglo y sumamos cada sueldo.
+            for (int i = 0; i < datos.Length; i++)
+            {
+                total = total + datos[i];
+            }
+            return total;
+        }
+
+        // Función para ordenar por burbuja
+        //Esta función ordena un arreglo de menor a mayor.
+        private void OrdenarBurbuja(double[] datos)
+        {
+            // Método burbuja ascendente: ordena de menor a mayor.
+            for (int i = 0; i < datos.Length - 1; i++)
+            {
+                for (int j = 0; j < datos.Length - 1 - i; j++)
+                {
+                     // Si el elemento actual es mayor que el siguiente, se intercambian.
+                    if (datos[j] > datos[j+1])
+                    {
+                        double auxiliar = datos[j];
+                        datos[j] = datos[j+1];
+                        datos[j + 1] = auxiliar;
+                    }
+                }
+            }
+        }
+
+        //Función para mostrar resultados - Mostrar un arreglo en el ListBox
+        //Esta función muestra los sueldos ordenados de un turno.
+        private void MostrarArreglo(string nombreTurno, double[] datos)
+        {
+            // Construimos una línea de texto con los sueldos del turno.
+            string linea = nombreTurno + ": ";
+            // Agregamos cada sueldo a la línea.
+            for (int i = 0; i < datos.Length; i++)
+            {
+                linea += $"S/{datos[i]:F2} ";
+            }
+            // Mostramos la línea en el ListBox.
+            lstResultados.Items.Add(linea);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Limpiamos resultados anteriores.
+                lstResultados.Items.Clear();
+                // 1. Leemos los sueldos de cada turno.
+                double[] manana = LeerSueldos(new TextBox[] { txbM1, txbM2, txbM3 });
+                double[] tarde = LeerSueldos(new TextBox[] { txbT1, txbT2 });
+                double[] noche = LeerSueldos(new TextBox[] { txbN1, txbN2 });
+                // 2. Calculamos el total de cada turno.
+                double totalManana = CalcularTotal(manana);
+                double totalTarde = CalcularTotal(tarde);
+                double totalNoche = CalcularTotal(noche);
+                // 3. Mostramos los totales.
+                lstResultados.Items.Add("=== TOTALES POR TURNO ===");
+                lstResultados.Items.Add($"Mañana: S/{totalManana:F2}");
+                lstResultados.Items.Add($"Tarde: S/{totalTarde:F2}");
+                lstResultados.Items.Add($"Noche: S/{totalNoche:F2}");
+
+                //4.Identificamos el turno con mayor gasto.
+                string turnoMayor = "Mañana";
+                double mayorGasto = totalManana;
+                if (totalTarde > mayorGasto)
+                {
+                    mayorGasto = totalTarde;
+                    turnoMayor = "Tarde";
+                }
+                if (totalNoche > mayorGasto)
+                {
+                    mayorGasto = totalNoche;
+                    turnoMayor = "Noche";
+                }
+                lblTurnoMayor.Text = $"Turno con mayor gasto: {turnoMayor} (S/{mayorGasto:F2})";
+                // 5. Ordenamos los sueldos de cada turno.
+                OrdenarBurbuja(manana);
+                OrdenarBurbuja(tarde);
+                OrdenarBurbuja(noche);
+                // 6. Mostramos los sueldos ordenados.
+                lstResultados.Items.Add("");
+                lstResultados.Items.Add("=== SUELDOS ORDENADOS DE MENOR A MAYOR ===");
+                MostrarArreglo("Mañana", manana);
+                MostrarArreglo("Tarde", tarde);
+                MostrarArreglo("Noche", noche);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese solo números válidos en los sueldos.",
+                "Dato no válido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message,
+                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiamos los TextBox.
+            txbM1.Clear();
+            txbM2.Clear();
+            txbM3.Clear();
+            txbT1.Clear();
+            txbT2.Clear();
+            txbN1.Clear();
+            txbN2.Clear();
+            // Limpiamos resultados.
+            lstResultados.Items.Clear();
+            // Reiniciamos el texto del Label.
+            lblTurnoMayor.Text = "____________________________";
+            // Enviamos el foco al primer TextBox.
+            txbM1.Focus();
+        }
+
+        private void btnDatosPrueba_Click(object sender, EventArgs e)
+        {
+            // Permite recargar los datos de prueba.
+            CargarDatosDePrueba();
+        }
+
+        private void CargarDatosDePrueba()
+        {
+            // Datos del turno mañana.
+            txbM1.Text = "1500";
+            txbM2.Text = "1200";
+            txbM3.Text = "1800";
+            // Datos del turno tarde.
+            txbT1.Text = "1100";
+            txbT2.Text = "1400";
+            // Datos del turno noche.
+            txbN1.Text = "1000";
+            txbN2.Text = "950";
+            // Limpiamos resultados anteriores.
+            lstResultados.Items.Clear();
+            lblTurnoMayor.Text = "Turno con mayor gasto: -";
+        }
+    }
+}
